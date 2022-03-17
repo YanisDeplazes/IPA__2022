7a96a485f2eb60dac9086c45b58e996517a16de9
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 14:40:02 +0100

    Adding Header in template-parts

---
+++ stage4/wp-content/themes/webdashboard/template-parts/header/header.php
@@ -0,0 +1,19 @@
+<header>
+   <div class="flex base__padding horizontal align-items__center align-items__space-between">
+      <div class="header__title flex align-items__center">
+         <div class="hamburger" onclick="toggleMainnavigation()">
+            <div></div>
+            <div></div>
+            <div></div>
+         </div>
+         <h4 class="inline-block">Web Dashboard</h4>
+      </div>
+      <div class="block-medium">
+         <form action="/" method="get" class="search flex align-items__center">
+            <input type="text" name="s" id="search" value="" />
+            <input type="image" alt="Search" src="../assets/img/search.svg" />
+         </form>
+      </div>
+      <div class="avatar"><img src="../assets/img/avatar.jpeg" alt="Avatar"></div>
+   </div>
+</header>
