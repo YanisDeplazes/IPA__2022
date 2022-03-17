425c09614737436a17d11003d8324fed3266bb57
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 17:56:38 +0100

    Adding Main__content component in template-parts

---
+++ stage4/wp-content/themes/webdashboard/template-parts/layout/main__content.php
@@ -0,0 +1,8 @@
+<?php 
+if ($args['key'] == 'start' ){
+    echo '<div class="main__content flex-1"><!--full-width for variable--><div class="base__padding vertical horizontal">';
+
+}else{
+    echo'</div></div></main>';
+}
+?>
