4faa09e73ad468d4ce285e79bbed536efb4b5dc9
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 18:02:27 +0100

    Adding the dynamic.css

---
+++ stage4/wp-content/themes/webdashboard/template-parts/layout/dynamiccss.php
@@ -0,0 +1,26 @@
+<style>
+#my4214::before {
+    -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
+    mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
+}
+
+#my3124::before {
+    -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/inhalte.svg') no-repeat;
+    mask: url('<?php echo get_template_directory_uri();?>/assets/images/inhalte.svg') no-repeat;
+}
+
+#my2141::before {
+    -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/personen.svg') no-repeat;
+    mask: url('<?php echo get_template_directory_uri();?>/assets/images/personen.svg') no-repeat;
+}
+
+#my1232::before {
+    -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/scrum.svg') no-repeat;
+    mask: url('<?php echo get_template_directory_uri();?>/assets/images/scrum.svg') no-repeat;
+}
+
+.navigation.secondary .block-small-only::before {
+    -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
+    mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
+}
+</style>
