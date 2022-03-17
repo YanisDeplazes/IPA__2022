1788c7c9f2a9c84385378c40505865993b73626b
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 18:03:05 +0100

    Adding Dynamic CSS to Header

--- stage4/wp-content/themes/webdashboard/header.php
+++ stage4/wp-content/themes/webdashboard/header.php
@@ -14,10 +14,12 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
-      <link rel="stylesheet" href="dynamic.css">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dynamic.css">
       <title>Layout</title>
 	  <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  <?php wp_head(); ?>
    </head>
    <body <?php body_class(); ?>>
-   <?php get_template_part( 'template-parts/header/header', 'header' ); ?>
+   <?php 
+   	get_template_part( 'template-parts/header/header', 'header' ); 
+	get_template_part( 'template-parts/layout/dynamiccss', 'dynamiccss' ); ?>
