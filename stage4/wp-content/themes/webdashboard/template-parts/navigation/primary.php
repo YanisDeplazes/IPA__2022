2b5706e2d54342af8a0fa53504d9f6ecfddf1fb3
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 14:39:44 +0100

    Adding Navigations in template-parts

---
+++ stage4/wp-content/themes/webdashboard/template-parts/navigation/primary.php
@@ -0,0 +1,10 @@
+<aside class="navigation primary">
+   <div class="base__padding vertical horizontal">
+      <ul id="navigation-primary">
+         <li id="my4214" class="active"><a>Übersicht</a></li>
+         <li id="my3124"><a href="lists/index.html">Inhalte</a></li>
+         <li id="my2141"><a href="project/index.html">Personen</a></li>
+         <li id="my1232"><a href="../fonts/index.html">Fonts</a></li>
+      </ul>
+   </div>
+</aside>
---
+++ stage4/wp-content/themes/webdashboard/template-parts/navigation/secondary.php
@@ -0,0 +1,13 @@
+<div class="navigation secondary site-margin">
+   <div class="base__padding horizontal vertical">
+      <div class="block-small-only active flex align-items__center">
+         <h5 class="active">Title 2</h5>
+         <img onclick="toggleSecondnavigation('mobile')" src="../assets/img/collapse.svg" class="collapse mobile" alt="Collapse">
+      </div>
+      <ul class="block-medium" id="secondnavigation">
+         <li class="active"><a>Kunden</a></li>
+         <li><a>Mitarbeiter</a></li>
+      </ul>
+      <img onclick="toggleSecondnavigation('desktop')" src="../assets/img/collapse.svg" class="collapse desktop" alt="Collapse">
+   </div>
+</div>
