8077917abc9c56470d8a1ca28135d0c6a1ad3484
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 18:02:42 +0100

    Adding Front Page

--- stage4/wp-content/themes/webdashboard/front-page.php
+++ stage4/wp-content/themes/webdashboard/front-page.php
@@ -1,21 +1,20 @@
 <?php
-/**
- * If there is a front-page.php file in the theme, this template is always used for the start page. Without the template file, either home.php (blog index) or page.php (static start page) is loaded as normal.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display
- *
- */
-
-get_header();?>
-<div class="page-content">
-    <?php if ( have_posts() ) : ?>
-        <?php while ( have_posts() ) : ?>
-            <?php the_post(); ?>
-                <!--Post Loop-->
-        <?php endwhile; ?>
-                <!--Post Navigation-->
-    <?php else : ?>
-                <!--No Post Found-->
-    <?php endif; ?>
-</div>
-<?php get_footer(); ?>
+   /**
+    * If there is a front-page.php file in the theme, this template is always used for the start page. Without the template file, either home.php (blog index) or page.php (static start page) is loaded as normal.
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display
+    *
+    */
+   
+   get_header();
+   echo '<main class="project">';
+   get_template_part( 'template-parts/navigation/primary', 'primary' ); 
+   get_template_part( 'template-parts/navigation/secondary', 'secondary' ) ;
+   get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'start') ); 
+   get_template_part( 'template-parts/layout/loader', 'loader'); 
+   get_template_part( 'template-parts/page/sections/sectionwrapper', 'sectionwrapper', array('key'   => 'intro' , 'box' => true , 'content'   => array('<h1>Hallo, Yanis Deplazes</h1><hr class="fullwidth">') ) ); 
+   get_template_part( 'template-parts/page/sections/sectionwrapper', 'sectionwrapper', array('key'   => 'versionstatus' , 'box' => true , 'title'   => 'Version Status', 'content'   => array("CMS", "Server") ) ); 
+   get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'end') ); 
+   get_footer(); 
+   
+   ?>
