6b5be2bc417a34dbe60da36f1992cbe74fe09250
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 18:01:48 +0100

    Create 404 Page

--- stage4/wp-content/themes/webdashboard/404.php
+++ stage4/wp-content/themes/webdashboard/404.php
@@ -1,14 +1,19 @@
 <?php
-/**
- * File Template for displaying Error 404 pages 
- * 
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- */
-
-get_header();
-?>
-<div class="page-content">
-	<p><?php esc_html_e( 'Nothing seems to have been found at this location. Maybe try a search?', 'sm-wp-theme' ); ?></p>
-	<?php get_search_form(); ?>
-</div>
-<?php get_footer(); ?>
+   /**
+    * If there is a front-page.php file in the theme, this template is always used for the start page. Without the template file, either home.php (blog index) or page.php (static start page) is loaded as normal.
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display
+    *
+    */
+   
+   get_header();
+   echo '<main class="project">';
+   get_template_part( 'template-parts/navigation/primary', 'primary' ); 
+   get_template_part( 'template-parts/navigation/secondary', 'secondary' ) ;
+   get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'start') ); 
+   get_template_part( 'template-parts/layout/loader', 'loader'); 
+   get_template_part( 'template-parts/page/sections/sectionwrapper', 'sectionwrapper', array('key'   => 'intro' , 'content'   => array('<h1>Error 404</h1><hr class="fullwidth">') ) ); 
+   get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'end') ); 
+   get_footer(); 
+   
+   ?>
