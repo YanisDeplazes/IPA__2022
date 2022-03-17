c69ea56f9cdf3616d8bbbe982ddbbaa5def5e9d5
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-17 14:40:31 +0100

    Applying dynamic Header

--- stage4/wp-content/themes/webdashboard/header.php
+++ stage4/wp-content/themes/webdashboard/header.php
@@ -7,11 +7,17 @@
  */
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+      <link rel="stylesheet" href="dynamic.css">
+      <title>Layout</title>
+	  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+	  <?php wp_head(); ?>
+   </head>
+   <body <?php body_class(); ?>>
+   <?php get_template_part( 'template-parts/header/header', 'header' ); ?>
