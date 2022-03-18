4cfa98537a4cf7d7184a1e8705a11203019d4100
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-18 14:18:05 +0100

    Adding Custom Login Page

--- stage4/wp-content/themes/webdashboard/functions.php
+++ stage4/wp-content/themes/webdashboard/functions.php
@@ -11,4 +11,10 @@
 if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
 }
+
+function my_custom_login(){
+	echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') .'/login/custom-login-style.css"/>';
+}
+add_action('login_head','my_custom_login');
+
 ?>
