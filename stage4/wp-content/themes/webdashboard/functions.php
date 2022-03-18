07af225d80834842c2cc2968ae7824293bee48da
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-18 14:23:32 +0100

    Adding Force Login via tutorial

--- stage4/wp-content/themes/webdashboard/functions.php
+++ stage4/wp-content/themes/webdashboard/functions.php
@@ -17,4 +17,24 @@ function my_custom_login(){
 }
 add_action('login_head','my_custom_login');
 
+// Force Login https://www.trickspanda.com/force-users-login-viewing-wordpress/
+function v_getUrl() {
+	$url  = isset( $_SERVER['HTTPS'] ) && 'on' === $_SERVER['HTTPS'] ? 'https' : 'http';
+	$url .= '://' . $_SERVER['SERVER_NAME'];
+	$url .= in_array( $_SERVER['SERVER_PORT'], array('80', '443') ) ? '' : ':' . $_SERVER['SERVER_PORT'];
+	$url .= $_SERVER['REQUEST_URI'];
+	return $url;
+  }
+  function v_forcelogin() {
+	if( !is_user_logged_in() ) { // Check if User is not logged in
+	  $url = v_getUrl(); // Get URL
+	  $whitelist = apply_filters('v_forcelogin_whitelist', array()); 
+	  $redirect_url = apply_filters('v_forcelogin_redirect', $url);
+	  if( preg_replace('/\?.*/', '', $url) != preg_replace('/\?.*/', '', wp_login_url()) && !in_array($url, $whitelist) ) {
+		wp_safe_redirect( wp_login_url( $redirect_url ), 302 ); exit();
+	  }
+	}
+  }
+  add_action('init', 'v_forcelogin');
+
 ?>
