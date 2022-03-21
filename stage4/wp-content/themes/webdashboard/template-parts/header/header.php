f4ffb5916982d6453a42cf5850c78a9e9c7c3763
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-21 08:52:13 +0100

    Adding Website Title

--- stage4/wp-content/themes/webdashboard/template-parts/header/header.php
+++ stage4/wp-content/themes/webdashboard/template-parts/header/header.php
@@ -2,6 +2,6 @@
 $current_user = wp_get_current_user();
 $avatar_url = get_avatar( $current_user->ID, 32 );
 
-echo '<header><div class="flex base__padding horizontal align-items__center align-items__space-between"><div class="header__title flex align-items__center"><div class="hamburger" onclick="toggleMainnavigation()"><div></div><div></div><div></div></div><h4 class="inline-block">Web Dashboard</h4></div><div class="block-medium">';
+echo '<header><div class="flex base__padding horizontal align-items__center align-items__space-between"><div class="header__title flex align-items__center"><div class="hamburger" onclick="toggleMainnavigation()"><div></div><div></div><div></div></div><h4 class="inline-block">'. get_bloginfo() .'</h4></div><div class="block-medium">';
 echo get_search_form();
 echo '</div><div class="avatar">' . $avatar_url . '</div></div></header>'; ?>
