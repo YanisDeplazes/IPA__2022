b4b883f4b55de3229c9315ee7ab521f668b1f2e5
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-22 09:51:40 +0100

    Single Elements

---
+++ stage4/wp-content/themes/webdashboard/taxonomy-objekte-servers.php
@@ -0,0 +1,65 @@
+<?php
+   /**
+    * File Template for displaying archive pages. You can display archive title (tag, category, date-based, or author archives).
+    * There are other archive files you can use, see the archive structure: https://developer.wordpress.org/files/2014/10/Screenshot-2019-01-23-00.20.04.png
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+    */
+      
+      get_header();
+      $current_user = wp_get_current_user(); /* Get User */
+      $user_name = $current_user->user_firstname . ' ' . $current_user->user_lastname; /* First + Lastname */
+      
+      echo '<main class="project">';
+      get_template_part( 'template-parts/navigation/primary', 'primary' ); /* Loading Primary Navigation */
+      get_template_part( 'template-parts/navigation/secondary', 'secondary', array('key'   => 'objekte') ) ; /* Loading Secondary Navigation */
+      get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'start', 'fullwidth'   => false) );  /* Loading Beginning of main__Content */
+      get_template_part( 'template-parts/layout/loader', 'loader'); /* Loading Loader  */?>
+<div class="box loop main">
+   <div class="loop-wrapper">
+      <table class="loop">
+         <thead>
+            <tr>
+               <th class="overline">Firma</th>
+               <th class="overline">Ansprechperson</th>
+               <th class="overline">Projekte</th>
+               <th class="overline"></th>
+            </tr>
+         </thead>
+         <?php if ( have_posts() ) : ?>
+         <?php while ( have_posts() ) : the_post();?>
+         <tbody>
+            <tr onclick="window.location.href='../single/index.html'">
+               <td class="order1">
+                  <h6><a href="../single/index.html" class="nostyle"><?php the_title();?></a></h6>
+               </td>
+               <td class="order2"><a href="../single/index.html" class="nostyle"><?php the_title();?></a></td>
+               <td class="order3"><?php the_title();?></td>
+               <td class="right-text order4">
+                  <div class="details-wrapper">
+                     <a href="../single/index.html" class="nostyle">
+                        <div class="details"><span></span><span></span><span></span></div>
+                     </a>
+                  </div>
+               </td>
+            </tr>
+         </tbody>
+         <?php endwhile; ?>
+      </table>
+   </div>
+   <div class="table__footer right-text overline">
+      <div class="base__padding horizontal">
+         <div class="pagination flex align-items__center">
+            <div>Seite 2 von 5</div>
+            <img src="../assets/img/previous.svg" alt="Previous"> <img src="../assets/img/next.svg" alt="Next">
+         </div>
+      </div>
+   </div>
+</div>
+<?php else : ?>
+<!--No Post Found-->
+<?php endif; ?>
+<?php get_template_part( 'template-parts/layout/main__content', 'main__content', array('key'   => 'end') );  /* Loading End of main__Content */
+   get_footer(); 
+   
+   ?>
--- stage4/wp-content/themes/webdashboard/template-parts/layout/dynamiccss.php
+++ stage4/wp-content/themes/webdashboard/template-parts/layout/dynamiccss.php
@@ -5,6 +5,7 @@ $menu_icon = get_field( "menu_icon",  $id);
 ?>
 
 <style>
+<<<<<<< Updated upstream
 .inhalte #menu-item-93::before, .personen #menu-item-79::before{
     background-color: #009BA9;
 }
@@ -19,17 +20,27 @@ $menu_icon = get_field( "menu_icon",  $id);
     border-top-right-radius: 3px;
     border-bottom-right-radius: 3px;
 }
+=======
+>>>>>>> Stashed changes
 #menu-item-60::before {
     -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
     mask: url('<?php echo get_template_directory_uri();?>/assets/images/uebersicht.svg') no-repeat;
 }
 
+<<<<<<< Updated upstream
 #menu-item-93::before {
+=======
+#menu-item-59::before {
+>>>>>>> Stashed changes
     -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/inhalte.svg') no-repeat;
     mask: url('<?php echo get_template_directory_uri();?>/assets/images/inhalte.svg') no-repeat;
 }
 
+<<<<<<< Updated upstream
 #menu-item-79::before {
+=======
+#menu-item-58::before {
+>>>>>>> Stashed changes
     -webkit-mask: url('<?php echo get_template_directory_uri();?>/assets/images/personen.svg') no-repeat;
     mask: url('<?php echo get_template_directory_uri();?>/assets/images/personen.svg') no-repeat;
 }
