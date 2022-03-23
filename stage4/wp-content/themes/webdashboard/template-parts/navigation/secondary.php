f3a0641c0d64dc520b4da22db9134d0004d09662
Yanis Deplazes <34231877+YanisDeplazes@users.noreply.github.com>
2022-03-23 10:09:15 +0100

    Removing Side Padding on Search on Mobile

--- stage4/wp-content/themes/webdashboard/template-parts/navigation/secondary.php
+++ stage4/wp-content/themes/webdashboard/template-parts/navigation/secondary.php
@@ -5,10 +5,12 @@
    *
    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
    */
-echo '<div class="navigation secondary site-margin"><div class="base__padding horizontal vertical"><div class="block-small-only current-menu-item flex align-items__center"><h5 class="current-menu-item">Title 2</h5><img onclick="toggleSecondnavigation(\'mobile\')" src="' . get_template_directory_uri() .'/assets/images/collapse.svg" class="collapse mobile" alt="Collapse"></div>';
-      
-      $key = $args['key']; 
 
+   $key = $args['key']; 
+   $title = $args['term'];
+
+   echo '<div class="navigation secondary site-margin"><div class="base__padding horizontal vertical"><div class="block-small-only current-menu-item flex align-items__center"><h5 class="current-menu-item">'.$title.'</h5><img onclick="toggleSecondnavigation(\'mobile\')" src="' . get_template_directory_uri() .'/assets/images/collapse.svg" class="collapse mobile" alt="Collapse"></div>';
+      
       //Check What Navigation should be used
       if ($key == 'personen'){
          wp_nav_menu( array('theme_location' => 'navigation-secondary-personen',) ); 
